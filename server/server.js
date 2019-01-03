4f7f632276b51b81797acaf219dd72eba12e931b
cherrih <cherri.hartigan@gmail.com>
2019-01-02 19:35:50 -0800

    Render product-photo and summary components

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 3000;
 app.use(morgan('dev'));
 app.use('/watches/:wid', express.static('public'));
 
+app.use('/api/watches/:wid/photos', proxy({ target: 'http://localhost:3001', changeOrigin: true }));
 app.use('/api/watches/:wid/summary', proxy({ target: 'http://localhost:3002', changeOrigin: true }));
 
 
