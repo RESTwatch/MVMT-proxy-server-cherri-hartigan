5bc29410eb11932aceceeb673ccf845b74c6c099
cherrih <cherri.hartigan@gmail.com>
2019-01-02 17:26:55 -0800

    Render Summary component

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const proxy = require('http-proxy-middleware');
+
+const port = process.env.PORT || 3000;
+
+app.use(morgan('dev'));
+app.use('/watches/:wid', express.static('public'));
+
+app.use('/api/watches/:wid/summary', proxy({ target: 'http://localhost:3002', changeOrigin: true }));
+
+
+app.listen(port, () => {
+  console.log(`server running at: http://localhost:${port}`);
+});
