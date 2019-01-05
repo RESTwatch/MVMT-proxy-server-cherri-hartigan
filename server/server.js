3b32042d7e49101c966d32620df6ceb035dd5391
cherrih <cherri.hartigan@gmail.com>
2019-01-04 21:01:25 -0800

    Proxy dockerized for deployment

--- server/server.js
+++ server/server.js
@@ -8,9 +8,10 @@ const port = process.env.PORT || 3000;
 app.use(morgan('dev'));
 app.use('/watches/:wid', express.static('public'));
 
-app.use('/api/watches/:wid/photos', proxy({ target: 'http://localhost:3001', changeOrigin: true }));
-app.use('/api/watches/:wid/summary', proxy({ target: 'http://localhost:3002', changeOrigin: true }));
-app.use('/api/watches/:wid/details', proxy({ target: 'http://localhost:3003', changeOrigin: true }));
+app.use('/api/watches/:wid/photos', proxy({ target: 'http://54.211.73.211', changeOrigin: true }));
+app.use('/api/watches/:wid/summary', proxy({ target: 'http://54.87.165.253', changeOrigin: true }));
+app.use('/api/watches/:wid/details', proxy({ target: 'http://13.57.38.154', changeOrigin: true }));
+app.use('/api/watches/:wid/reviews', proxy({ target: 'http://18.218.241.2', changeOrigin: true }));
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
